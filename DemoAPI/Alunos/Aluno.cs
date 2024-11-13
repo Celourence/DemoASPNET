ba0f509dc3daebc6d6c0abcd7285588c35b1bf0b
Christian <celourence@gmail.com>
2024-11-13 18:56:07 -0300

    [Commit 07] Add DemoAPI project

---
+++ DemoAPI/Alunos/Aluno.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DemoAPI.Alunos
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public String? Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataCadastro { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
