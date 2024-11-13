d05eaac7ab63f3cfafcf4d203351aff97c9ee217
Christian <celourence@gmail.com>
2024-11-13 18:53:13 -0300

    [Commit 05] Add DemoRazorPages project

---
+++ DemoRazorPages/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DemoRazorPages.Pages.Alunos;
+
+namespace DemoRazorPages.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<DemoRazorPages.Pages.Alunos.Aluno> Aluno { get; set; } = default!;
+    }
+}
---
+++ DemoRazorPages/Pages/Alunos/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoRazorPages.Pages.Alunos
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DemoRazorPages.Data.ApplicationDbContext _context;
+
+        public IndexModel(DemoRazorPages.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Aluno> Aluno { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Aluno = await _context.Aluno.ToListAsync();
+        }
+    }
+}
